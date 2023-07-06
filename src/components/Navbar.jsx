ad3d049478b61e18e8d01c8c1b0bbd9f3b742005
adar tulloch <tullochadar@gmail.com>
2023-07-06 14:20:06 -0400

    [ui] add sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,9 +19,10 @@ import {
 } from "@mui/material";
 import FlexBetween from "./FlexBewteen";
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
+  console.log(isSidebarOpen);
   return (
     <AppBar
       sx={{
@@ -33,7 +34,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -3,11 +3,25 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
+
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  console.log(isNonMobile);
   return (
-    <Box width="100%" height="100%">
-      <Box>
-        <Navbar />
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
