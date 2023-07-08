b19687776cd99ab02661f3ffdccb5592aa36ab37
adar tulloch <tullochadar@gmail.com>
2023-07-08 14:01:32 -0400

    [ui] add products

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Dashboard from "../src/scenes/dashboard/index";
 import Layout from "../src/scenes/layout/index";
+import Products from "../src/scenes/products/index";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -19,6 +20,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
--- src/state/api.js
+++ src/state/api.js
@@ -6,11 +6,16 @@ export const api = createApi({
   }),
   reducerPath: "adminApi",
   tagTypes: ["User"],
-  endpoints: (build) => ({
-    getUser: build.query({
+  endpoints: (builder) => ({
+    getUser: builder.query({
       query: (id) => `general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: builder.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
     }),
   }),
 });
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
